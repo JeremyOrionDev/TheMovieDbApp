244eb78c6c314c138c70d638c37c20e037427fa0
ORION jeremy <jeremyoriondev@gmail.com>
2017-10-18 16:09:57 +0200

    Ajoutez des fichiers projet.

---
+++ MusicStoryAPIApp/www/scripts/detailsFilms.js
@@ -0,0 +1,126 @@
+﻿// Pour obtenir une présentation du modèle Vide, consultez la documentation suivante :
+// http://go.microsoft.com/fwlink/?LinkID=397704
+// Pour déboguer du code durant le chargement d'une page dans cordova-simulate ou sur les appareils/émulateurs Android, lancez votre application, définissez des points d'arrêt, 
+// puis exécutez "window.location.reload()" dans la console JavaScript.
+var id = getUrlVars()["id"];
+console.log("id= ", id);
+var movieId;
+var listeVideo = new Array();
+        var $Retour = $('#retour');
+function getUrlVars() {
+    var vars = [], hash; // Déclaration d'un tableau vars et d’une variable "hash"
+
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&'); /* split des paramètres après le "?" de l'url ; les chaînes de
+        caractères possèdent une méthode split() qui permet de les découper en un tableau,
+        en fonction d'un séparateur précisé.*/
+    console.log("les hashes", hashes);
+    console.log('le hashes length = ', hashes.length);
+    for (var i = 0; i < hashes.length; i++) // boucle sur le nombre de paramètres
+    {
+        console.log('le hash brut ', hashes);
+        hash = hashes[i].split('='); // split des paires nom et valeur du paramètre
+        console.log('le hash = ', hash);
+        console.log("le hash 0 ", hash[0]);
+        console.log('le hash 1: ', hash[1]);
+        vars.push(hash[0]);
+        /* La méthode push() permet d'ajouter un ou plusieurs items à un tableau,
+        push() peut recevoir un nombre illimité de paramètres, et chaque paramètre
+        représente un item à ajouter à la fin du tableau.
+        La méthode unshift() fonctionne comme push(), excepté que les items sont ajoutés
+        au début du tableau. Cette méthode n'est pas très fréquente mais peut être utile.
+        Les méthodes shift() et pop() retirent respectivement le premier et le dernier
+        élément du tableau. */
+        vars[hash[0]] = hash[1]; // insertion des valeurs dans le tableau
+    }
+    return vars; // retourne le tableau de paramètres
+};
+$Retour.css('border-bottom-right-radius', '100px');
+$Retour.css('z-index', '-10');
+(function () {
+    "use strict";
+
+    document.addEventListener('deviceready', onDeviceReady.bind(this), false);
+
+    function onDeviceReady() {
+        // Gérer les événements de suspension et de reprise Cordova
+        document.addEventListener('pause', onPause.bind(this), false);
+        document.addEventListener('resume', onResume.bind(this), false);
+
+
+       
+    };
+   
+    function onPause() {
+        // TODO: cette application a été suspendue. Enregistrez l'état de l'application ici.
+    };
+
+    function onResume() {
+        // TODO: cette application a été réactivée. Restaurez l'état de l'application ici.
+    };
+})();
+(function infoFilm() {
+    $.ajax({
+
+        url: "https://api.themoviedb.org/3/movie/" + id + "?api_key=85a1114cc9bcee8c748abdaaade8169a&language=fr-FR",
+        cache: false,
+        type: "GET",
+        dataType: "json",
+        success: function (data) {
+
+
+            console.log("les data du films::", data);
+            $('#listFilm').append("<p class='panel'>" + data.original_title + "</p><img class='col-10' src='https://image.tmdb.org/t/p/w500/" + data.poster_path + "' alt=" + data.original_title + " /><p style='font-size:0.5em'> Synopsis : </br><span style='font-size:0.7em'>" + data.overview + "</span></p><p style='font-size:0.5em'> Sortie :  " + data.release_date + "</p>");
+            
+        }
+    });
+    
+})();
+(function getMovies() {
+    $.ajax({
+
+        url: "https://api.themoviedb.org/3/movie/" + id + "/videos?api_key=85a1114cc9bcee8c748abdaaade8169a&language=en-EN",
+        cache: false,
+        type: "GET",
+        dataType: "json",
+        success: function (data) {
+            for (var i = 0; i < data.results.length; i++) {
+                $('#player').append('<iframe class="col-12" id="player" type="text/html" src= "http://www.youtube.com/embed/'+data.results[i].key+'?enablejsapi=1&origin=http://example.com" frameborder= "0" ></iframe >');
+            }
+            onYouTubePlayerAPIReady;
+            console.log('les vides en liste: ',listeVideo);
+            console.log("les data pour la video: ",data.results.length);
+            console.log("id video::", data.key);
+
+        }
+    });
+})();
+// Replace the 'ytplayer' element with an <iframe> and
+// YouTube player after the API code downloads.
+var player;
+function onYouTubePlayerAPIReady() {
+    player = new YT.Player('player', {
+        height: auto,
+        width: auto,
+        disablekb: 0,
+        fs: 1,
+        videoId: 'M7lc1UVf-VE'
+    });
+};
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+    event.target.playVideo();
+};
+
+// 5. The API calls this function when the player's state changes.
+//    The function indicates that when playing a video (state=1),
+//    the player should play for six seconds and then stop.
+var done = false;
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+        setTimeout(stopVideo, 6000);
+        done = true;
+    }
+};
+function stopVideo() {
+    player.stopVideo();
+};
---
+++ MusicStoryAPIApp/www/scripts/index.js
@@ -0,0 +1,157 @@
+﻿// Pour obtenir une présentation du modèle Vide, consultez la documentation suivante :
+// http://go.microsoft.com/fwlink/?LinkID=397704
+// Pour déboguer du code durant le chargement d'une page dans cordova-simulate ou sur les appareils/émulateurs Android, lancez votre application, définissez des points d'arrêt, 
+// puis exécutez "window.location.reload()" dans la console JavaScript.
+var catId;
+var nomCategorie;
+var lesCat = new Array();
+var laCat;
+var adress;
+var lesFilms = new Array();
+var idPhoto = new Array();
+var idPop = new Array();
+(function () {
+    "use strict";
+
+    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+    
+    function onDeviceReady() {
+        // Gérer les événements de suspension et de reprise Cordova
+        document.addEventListener( 'pause', onPause.bind( this ), false );
+        document.addEventListener( 'resume', onResume.bind( this ), false );
+        var $afficher = $('#btn'),
+            $list = $('#listCat'),
+            $pop = $('#popBtn');
+
+        console.log(catId);
+        $pop.click(function () {
+            filmPopulaires();
+        })
+        $afficher.click(function () {
+            alert('click affiche');
+            listerFilms();
+        });
+
+
+        $list.change(function () {
+            nomCategorie = $("#listCat option:selected").text();
+            
+            console.log("longueur tableau: ",lesCat.length);
+            for (var i = 0; i < lesCat.length; i++) {
+                console.log("tablo: " + lesCat[i][0]);
+                console.log("le nom ",lesCat[i]['name']);
+                if (lesCat[i]['name']=== $('#listCat option:selected').text()) {
+                    laCat = lesCat[i]['name'];
+                    catId = lesCat[i]['id'];
+                    console.log("mon id choisi:: ",catId);
+                    listerFilms(lesCat[i]['id']);
+                    
+                }
+            }
+            console.log(lesCat);
+            catId = document.getElementById('listCat').options[document.getElementById('listCat').selectedIndex].value;
+            //alert(nomCategorie);
+            //alert("le num = ",num);
+        });
+    };
+    (function listeCategories() {
+        console.log("le numero :: ",catId);
+        //alert('fct ok');
+        $.ajax({
+            url: "https://api.themoviedb.org/3/genre/movie/list?api_key=85a1114cc9bcee8c748abdaaade8169a&language=en-US",
+            cache: false,
+            type: "GET",
+            dataType: "json",
+            success: function (data) {
+                lesCat = data.genres;
+                //lesCat = data.genres;
+                console.log("tableau cat::", lesCat);
+                console.log("les genres: ", data.genres[2].name);
+                for (var i = 0; i < data.genres.length; i++) {
+                    
+                    console.log(laCat);
+                    var genre = data.genres[i].name;
+                    //var adress = "https://image.tmdb.org/t/p/w500/" + data.results[i].backdrop_path;
+                    $('#listCat').append($('<option>', { value: i, text: genre.toString() }));
+                    //$('#listFilm').append(" <option value="+data.genres[i].name+">"+data.genres[i].name+"</option>");
+                }
+            
+            }
+        });
+    })();
+    function listerFilms(id) {
+        //alert("fct listerFilms");
+        $.ajax({
+            url: "https://api.themoviedb.org/3/genre/" + id + "/movies?api_key=85a1114cc9bcee8c748abdaaade8169a&language=en-US&include_adult=false&sort_by=created_at.asc",
+            cache: false,
+            type: "GET",
+            dataType: "json",
+            success: function (data) {
+                console.log("les data des films::", data);
+                console.log("nombre de films trouvés", data.total_results);
+                lesFilms = data.object_ids;
+                for (var i = 0; i < data.results.length; i++) {
+                    console.log("la page 2: ",data.results['page'][2]);
+                console.log("les id films", data.results[i]['original_title']);
+                    //var leFilm = getFilm(data.object_ids[0][0]);
+                }
+            }
+        });
+    };
+    function filmPopulaires() {
+        for (var j = 0; j < 50; j++) {
+
+            $.ajax({
+                url: "https://api.themoviedb.org/3/movie/popular?api_key=85a1114cc9bcee8c748abdaaade8169a&language=en-US/&page="+j,
+                cache: false,
+                type: "GET",
+                dataType: "json",
+                success: function (data) {
+                    console.log("les data des films populaires::", data);
+                    console.log("nombre de films trouvés", data.total_results);
+                    lesFilms = data.object_ids;
+                    for (var i = 0; i < data.results.length; i++) {
+                        idPop.push(data.results[i].id);
+                            idPhoto.push(data.results[i].id);
+                            if (data.results[i].backdrop_path==null) {
+                                $('img').css('display', 'none');
+                            } else var laPhoto = "https://image.tmdb.org/t/p/w500/" + data.results[i].backdrop_path;
+                            console.log("adresse photo: ", laPhoto);
+                            $('#listFilm').append("<img class='col-6'  id=" + data.results[i].id + " href='detailsFilms.html?id=" + data.results[i].id + "' src=" + laPhoto + "  alt=" + data.results[i].name + " />");
+                            
+       
+
+
+                        //var leFilm = getFilm(data.object_ids[0][0]);
+                    }
+                    for (var x = 0; x < length; x++) {
+
+                    }
+                console.log("les photo : ",idPhoto);
+                }
+            });
+
+        }
+    }
+    $('body').on('click', 'img', function () { location="detailsFilms.html?id="+this.id })
+    function getFilm(id) {
+        $.ajax({
+            
+            url: "https://api.themoviedb.org/3/movie/" + id + "?api_key=85a1114cc9bcee8c748abdaaade8169a&language=fr-FR",
+            cache: false,
+            type: "GET",
+            dataType: "json",
+            success: function (data) {
+                console.log("les data du films::", data);
+                $('#listFilm').append("<p> " + data.name + "</p>");
+            }
+        });
+    }
+    function onPause() {
+        // TODO: cette application a été suspendue. Enregistrez l'état de l'application ici.
+    };
+
+    function onResume() {
+        // TODO: cette application a été réactivée. Restaurez l'état de l'application ici.
+    };
+} )();
